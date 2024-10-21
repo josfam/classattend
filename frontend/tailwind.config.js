759605aa96075041b4039cfdaaebee7d74838518
josfam <jamukun@proton.me>
2024-10-21 10:12:39 +0300

    Re-setup frontend to use typescript

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Add support for TypeScript and JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
