1e797c046669b4868d5fb8436bb4ec7b60f03b17
josfam <jamukun@proton.me>
2024-11-07 01:07:10 +0300

    Format app and main files with eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,37 @@
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes
- } from 'react-router-dom'
-import { Toaster } from 'sonner';
-import SignupForm from './pages/SignUpForm';
-import LoginForm from './pages/LoginForm';
-import HomePage from './pages/HomePage';
-import StudentHome from './roles/layouts/student/pages/StudentHome';
-import LecturerHome from './roles/layouts/lecturer/pages/LecturerHome';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+import SignupForm from "./pages/SignUpForm";
+import LoginForm from "./pages/LoginForm";
+import HomePage from "./pages/HomePage";
+import StudentHome from "./roles/layouts/student/pages/StudentHome";
+import LecturerHome from "./roles/layouts/lecturer/pages/LecturerHome";
 
 const App = () => {
   return (
-  <>
-    <Toaster richColors position='top-center'/> {/* toast messages */}
-    <div className='app-content w-full h-full p-8 flex items-center justify-center bg-neutral-50
-    overflow-y-scroll'>
-      <Router>
-        <Routes>
-          {/* public routes */}
-          <Route index element={<HomePage/>}/>
-          <Route path='/signup' element={<SignupForm/>}/>
-          <Route path='/login' element={<LoginForm/>}/>
+    <>
+      <Toaster richColors position="top-center" /> {/* toast messages */}
+      <div className="app-content flex h-full w-full items-center overflow-y-scroll bg-neutral-50 p-8 first-letter:justify-center">
+        <Router>
+          <Routes>
+            {/* public routes */}
+            <Route index element={<HomePage />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm />} />
 
-          {/* student-specific routes */}
-          <Route path='/student/*'>
-            <Route index element={<StudentHome/>}></Route>
-          </Route>
+            {/* student-specific routes */}
+            <Route path="/student/*">
+              <Route index element={<StudentHome />}></Route>
+            </Route>
 
-          {/* lecturer-specific routes */}
-          <Route path='/lecturer/*'>
-            <Route index element={<LecturerHome/>}></Route>
-          </Route>
-        </Routes>
-      </Router>
-    </div>
-  </>
+            {/* lecturer-specific routes */}
+            <Route path="/lecturer/*">
+              <Route index element={<LecturerHome />}></Route>
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
-}
+};
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './base.css'
-import './index.css'
-import './App.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./base.css";
+import "./index.css";
+import "./App.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
