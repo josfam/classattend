98d0b20deb5fa4539a7d082940e938b9a7c54452
josfam <jamukun@proton.me>
2024-11-07 01:09:11 +0300

    Format userStore with eslint

--- frontend/src/store/userStore.ts
+++ frontend/src/store/userStore.ts
@@ -2,24 +2,24 @@
  * Managing global user state with zustand
  */
 
-import { create } from 'zustand';
-import { Role } from '@/utils/schemas/SchemaConstants';
+import { create } from "zustand";
+import { Role } from "@/utils/schemas/SchemaConstants";
 
 // role type definition, as a union of the enum values
-type RoleType = typeof Role[keyof typeof Role];
+type RoleType = (typeof Role)[keyof typeof Role];
 
 // zustand store definition
 interface UserStore {
-	role: RoleType | null; // null at the start
-	setRole: (newRole: RoleType) => void; // set the new role
-	clearRole: () => void; // clear a role
+  role: RoleType | null; // null at the start
+  setRole: (newRole: RoleType) => void; // set the new role
+  clearRole: () => void; // clear a role
 }
 
 // zustand store creation
 const useUserStore = create<UserStore>((set) => ({
-	role: null, // starts as null initially
-	setRole: (newRole) => set({ role: newRole}),
-	clearRole: () => set({ role: null }),
+  role: null, // starts as null initially
+  setRole: (newRole) => set({ role: newRole }),
+  clearRole: () => set({ role: null }),
 }));
 
 export default useUserStore;
