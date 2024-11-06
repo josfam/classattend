bf4f6c1eefb9094e139d57f676d55c7be9bff218
josfam <jamukun@proton.me>
2024-11-06 18:13:29 +0300

    Add seperate configs for development & production

--- backend/api/v1/app_factory.py
+++ backend/api/v1/app_factory.py
@@ -2,6 +2,7 @@
 
 """Creates and returns an application object"""
 
+import os
 from flask import Flask
 from flask_session import Session
 from flask_cors import CORS
@@ -14,17 +15,17 @@ from backend.models.classroom import Classroom
 from backend.models.student_classroom import StudentClassroom
 from .routes import auth_route
 from dotenv import load_dotenv
-import os
 from datetime import timedelta
+from .config import DevelopmentConfig, ProductionConfig
 
 load_dotenv()
 
-def create_app(configuration=None):
+def create_app():
     """Creates and returns the application object"""
     app = Flask(__name__)
 
     # setup cors for all routes
-    frontend_host = os.getenv('CLIENT_ADDRESS')
+    frontend_host = os.getenv('DEV_CLIENT_ADDRESS')
     print('frontend_host', frontend_host) # DEBUG
     CORS(
         app,
@@ -37,21 +38,12 @@ def create_app(configuration=None):
         },
         supports_credentials=True,
     )
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI_STRING')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # configs for flask session
-    app.config['SESSION_TYPE'] = 'sqlalchemy'
-    app.config['SESSION_SQLALCHEMY'] = db
-    # app.config['SESSION_USE_SIGNER'] = True # sign the cookie for security
-    app.config['SESSION_PERMANENT'] = False # sessions are temporary
-    app.config['SESSION_COOKIE_SAMESITE'] = 'None' # separate domain for frontend and backend
-    app.config['SESSION_COOKIE_SECURE'] = 'False' # False for dev
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=80)
-
-    # apply the configuration if it exists
-    if configuration:
-        app.config.update(configuration)
+    # apply a config based on environment
+    if os.getenv('APP_ENVIRONMENT') == 'development':
+        app.config.from_object(DevelopmentConfig)
+    else:
+        app.config.from_object(ProductionConfig)
 
     # initialize all extensions
     db.init_app(app)
---
+++ backend/api/v1/config.py
@@ -0,0 +1,31 @@
+"""For app configuration"""
+
+import os
+from datetime import timedelta
+from backend.models.engine.storage import db
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class BaseConfig():
+    """Base configuration from which all other configurations will inherit
+    from
+    """
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # flask session
+    SESSION_TYPE = 'sqlalchemy'
+    SESSION_SQLALCHEMY = db
+    SESSION_PERMANENT= False # sessions are temporary
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=80)
+
+class DevelopmentConfig(BaseConfig):
+    """Configuration for the development environment"""
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URI_STRING')
+    # flask session
+    SESSION_COOKIE_SECURE = False
+    # cors
+    SESSION_COOKIE_SAMESITE = 'None' # separate domain for frontend and backend
+
+class ProductionConfig(BaseConfig):
+    """Configuration for the production environment"""
+    SESSION_USE_SIGNER = True # sign the cookie for security
