32747a5d91e6b977a1ec170b020e44a16cebf746
josfam <jamukun@proton.me>
2024-11-06 06:45:14 +0300

    Add zustand deps, and setup state store

---
+++ frontend/src/store/userStore.ts
@@ -0,0 +1,25 @@
+/**
+ * Managing global user state with zustand
+ */
+
+import { create } from 'zustand';
+import { Role } from '@/utils/schemas/SchemaConstants';
+
+// role type definition, as a union of the enum values
+type RoleType = typeof Role[keyof typeof Role];
+
+// zustand store definition
+interface UserStore {
+	role: RoleType | null; // null at the start
+	setRole: (newRole: RoleType) => void; // set the new role
+	clearRole: () => void; // clear a role
+}
+
+// zustand store creation
+const useUserStore = create<UserStore>((set) => ({
+	role: null, // starts as null initially
+	setRole: (newRole) => set({ role: newRole}),
+	clearRole: () => set({ role: null }),
+}));
+
+export default useUserStore;
