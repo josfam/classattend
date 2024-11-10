b64722311af64f8fdc433f3ca33917b68dfd955a
josfam <jamukun@proton.me>
2024-11-10 19:27:41 +0300

    Use logout button component in lecturer sidebar

--- frontend/src/roles/layouts/lecturer/components/LecturerSidebar.tsx
+++ frontend/src/roles/layouts/lecturer/components/LecturerSidebar.tsx
@@ -1,13 +1,10 @@
-import { BiLogOut } from "react-icons/bi";
-import SideBarBtn from "../../base/components/buttons/SidebarBtn";
+import LogoutBtn from "../../base/components/buttons/LogoutBtn";
 
 const LecturerSidebar = () => {
   return (
     <nav className="flex h-screen w-56 flex-col overflow-hidden bg-sky-700">
       {/* buttons */}
-      <div id="bottom-nav" className="mt-auto">
-        <SideBarBtn text="Logout" Icon={BiLogOut} />
-      </div>
+      <LogoutBtn />
     </nav>
   );
 };
