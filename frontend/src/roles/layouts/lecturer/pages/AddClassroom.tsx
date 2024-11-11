052849e3f65e04611ad9a2ad8356ec9018d2763b
josfam <jamukun@proton.me>
2024-11-11 13:27:50 +0300

    Change form placeholder text

--- frontend/src/roles/layouts/lecturer/pages/AddClassroom.tsx
+++ frontend/src/roles/layouts/lecturer/pages/AddClassroom.tsx
@@ -1,6 +1,6 @@
 
 const AddClassroom = () => {
-  return <h1>Yes</h1>; // placeholder
+  return <h1 className="text-2xl">Placeholder for form</h1>; // placeholder
 };
 
 export default AddClassroom;
