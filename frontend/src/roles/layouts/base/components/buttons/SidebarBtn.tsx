d098b55507d5c1c4542a0eae7f7627de63d77ce2
josfam <jamukun@proton.me>
2024-11-11 05:50:52 +0300

    Shrink the sidebar and sidebar buttons

--- frontend/src/roles/layouts/base/components/buttons/SidebarBtn.tsx
+++ frontend/src/roles/layouts/base/components/buttons/SidebarBtn.tsx
@@ -1,13 +1,17 @@
 import { IconType } from "react-icons/lib";
 
-interface SideBarBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+interface SideBarBtnProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   Icon: IconType;
 }
 
 const SideBarBtn = ({ text, Icon, ...props }: SideBarBtnProps) => {
   return (
-    <button {...props} className="btn-pri border-t-w-3 border-t-1 flex h-14 flex-row items-center justify-center gap-6 rounded-none border-t-sky-500 bg-sky-700 text-xl">
+    <button
+      {...props}
+      className="btn-pri border-t-w-3 border-t-1 flex h-14 flex-row items-center justify-center gap-6 rounded-none border-t-sky-500 bg-sky-700 text-lg"
+    >
       <div className="h-6 w-6">{Icon && <Icon className="text-2xl" />}</div>
       {text}
     </button>
