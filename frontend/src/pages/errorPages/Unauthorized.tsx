6b8693520ac786db42e85abde72a74372dd16e8e
josfam <jamukun@proton.me>
2024-11-11 05:45:58 +0300

    Format Unauthorized.tsx page

--- frontend/src/pages/errorPages/Unauthorized.tsx
+++ frontend/src/pages/errorPages/Unauthorized.tsx
@@ -2,13 +2,17 @@ import { redirectDuration } from "@/utils/errors/errorConstants";
 
 const UnauthorizedPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full bg-sky-50">
-      <div className=" flex flex-col gap-2 w-fit h-fit p-8 bg-red-200 rounded-lg">
-        <p className="text-2xl text-red-700"> You are not authorized to view this page</p>
-        <p className="text-2xl text-red-700"> Redirecting you back in {`${redirectDuration / 1000}`} seconds... </p>
+    <div className="flex h-full w-full flex-col items-center justify-center bg-sky-50">
+      <div className="flex h-fit w-fit flex-col gap-2 rounded-lg bg-red-200 p-8">
+        <p className="text-2xl text-red-700">
+          You are not authorized to view this page
+        </p>
+        <p className="text-2xl text-red-700">
+          Redirecting you back in {`${redirectDuration / 1000}`} seconds...{" "}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default UnauthorizedPage;
