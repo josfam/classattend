66539643d7bc385744dd737cddbc9f1aaa5713b5
josfam <jamukun@proton.me>
2024-11-11 12:15:39 +0300

    Format LecturerStudentSchemas file

--- frontend/src/utils/schemas/LecturerStudentSchemas.tsx
+++ frontend/src/utils/schemas/LecturerStudentSchemas.tsx
@@ -2,51 +2,61 @@ import { z } from "zod";
 import { Role } from "./SchemaConstants";
 
 const SharedSchema = {
-	firstname: z.string()
-    .min(2, {message: "Must be longer than 2 characters"})
-    .max(50, {message: "Must be less than 50 characters"}),
-	lastname: z.string()
-    .min(2, {message: "Must be longer than 2 characters"})
+  firstname: z
+    .string()
+    .min(2, { message: "Must be longer than 2 characters" })
+    .max(50, { message: "Must be less than 50 characters" }),
+  lastname: z
+    .string()
+    .min(2, { message: "Must be longer than 2 characters" })
     .max(50, "Must be less than 50 characters"),
-	email: z.string()
-    .email("This is not a valid email"),
-	password: z.string(),
+  email: z.string().email("This is not a valid email"),
+  password: z.string(),
   passwordConfirmation: z.string(),
   role: z.enum([Role.Lecturer, Role.Student]),
-}
+};
 
 const LecturerSchema = z.object({
   ...SharedSchema,
   faculty: z.enum(
-    ["Science and Technology", "Law", "Business and Management", "Post Graduate Studies and Research", "Socio-Economic Sciences"],
-    {message: "Choose at least one faculty"}
+    [
+      "Science and Technology",
+      "Law",
+      "Business and Management",
+      "Post Graduate Studies and Research",
+      "Socio-Economic Sciences",
+    ],
+    { message: "Choose at least one faculty" },
   ),
-  title: z.enum(
-    ["Mr.", "Mrs.", "Ms.", "Madam", "Professor", "Doctor"],
-    {message: "Choose at least one title"}
-  ),
-  staffId: z.string()
-    .min(1,
-      {message: "Provide your staff id"}),
+  title: z.enum(["Mr.", "Mrs.", "Ms.", "Madam", "Professor", "Doctor"], {
+    message: "Choose at least one title",
+  }),
+  staffId: z.string().min(1, { message: "Provide your staff id" }),
   role: z.literal(Role.Lecturer),
 });
 
 const StudentSchema = z.object({
   ...SharedSchema,
-  studentId: z.string()
-    .min(1,
-      {message: "Provide your student id"}),
+  studentId: z.string().min(1, { message: "Provide your student id" }),
   role: z.literal(Role.Student),
 });
 
-const SignupFormSchema = z.discriminatedUnion("role", [LecturerSchema, StudentSchema]);
+const SignupFormSchema = z.discriminatedUnion("role", [
+  LecturerSchema,
+  StudentSchema,
+]);
 
 const LoginFormSchema = z.object({
   email: z.string().email({
-    message: "Enter a valid email"
+    message: "Enter a valid email",
   }),
-  password: z.string()
-  .min(1, {message: "A password is required"}),
-})
+  password: z.string().min(1, { message: "A password is required" }),
+});
 
-export { SharedSchema, LecturerSchema, StudentSchema, SignupFormSchema, LoginFormSchema };
+export {
+  SharedSchema,
+  LecturerSchema,
+  StudentSchema,
+  SignupFormSchema,
+  LoginFormSchema,
+};
