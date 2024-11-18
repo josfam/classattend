0e105a873d1fa92d182a3396dd18b82a5380dff3
josfam <jamukun@proton.me>
2024-11-18 08:49:14 +0300

    Add backend logic for adding a classroom

--- backend/api/v1/routes/lecturer.py
+++ backend/api/v1/routes/lecturer.py
@@ -9,7 +9,34 @@ from backend.models.classroom import Classroom
 def get_classrooms():
     lecturer_id = session['user_id']
     # see if this lecturer already has classrooms
-    existing_classrooms = Classroom.query.filter_by(lecturer_id=lecturer_id).all()
+    existing_classrooms = Classroom.query.filter_by(
+        lecturer_id=lecturer_id
+    ).all()
     if not existing_classrooms:
         return jsonify({"message": "No classrooms found", "data": []}), 200
     return jsonify({"message": "classes received"}), 200
+
+
+@lecturer_route.route('/addclassroom', methods=['POST'], strict_slashes=False)
+def add_classroom():
+    data: dict = request.get_json()
+    class_name = data.get('className')
+    class_code = data.get('classCode')
+    class_description = data.get('classDescription')
+    lecturer_id = session.get('user_id')
+
+    # check if this class already exists
+    existing_class = Classroom.query.filter_by(code=class_code).first()
+    if existing_class:
+        return jsonify({'message': 'This classroom already exists'}), 409
+    # create a classroom
+    classroom = Classroom(
+        code=class_code,
+        name=class_name,
+        description=class_description,
+        lecturer_id=lecturer_id,
+    )
+    db.session.add(classroom)
+    db.session.commit()
+
+    return jsonify({'message': 'Classroom added successfully'}), 200
