ef47a92712515888949deb8c12d52baf34f67b3c
josfam <jamukun@proton.me>
2024-11-18 06:05:49 +0300

    Show success toasts in student home

--- frontend/src/roles/layouts/student/pages/StudentHome.tsx
+++ frontend/src/roles/layouts/student/pages/StudentHome.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { SuccessToast } from "@/components/Toasts";
+
 const StudentHome = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.showSuccessToast) {
+      SuccessToast({ message: location.state.message });
+    }
+  }, [location.state]);
+
+  useEffect(() => {
+    // clear state
+    navigate(location.pathname, { replace: true });
+  }, [location.pathname, navigate]);
+
   return <h1 className="text-3xl">Your student homepage</h1>;
 };
 
