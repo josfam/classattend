8c6e64e0723408b3b92573cc1478db279c01333d
josfam <jamukun@proton.me>
2024-11-18 14:20:55 +0300

    Make items start from the very top of the page

--- frontend/src/roles/layouts/lecturer/LecturerLayout.tsx
+++ frontend/src/roles/layouts/lecturer/LecturerLayout.tsx
@@ -9,7 +9,7 @@ const LecturerLayout = () => {
       {/* Sidebar */}
       <LecturerSidebar />
       {/* Main content area */}
-      <main className="flex h-screen w-full flex-col items-center justify-center overflow-y-scroll px-8 bg-white">
+      <main className="flex h-screen w-full flex-col items-center overflow-y-scroll bg-white px-8 pt-8">
         <Outlet /> {/* Rendering nested routes */}
       </main>
     </div>
