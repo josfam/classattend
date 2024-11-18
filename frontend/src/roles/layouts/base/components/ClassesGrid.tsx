17da7bc60c21a30401ce1fa667cedc561ae3b7bc
josfam <jamukun@proton.me>
2024-11-18 14:01:11 +0300

    Rearrange elements on class card

--- frontend/src/roles/layouts/base/components/ClassesGrid.tsx
+++ frontend/src/roles/layouts/base/components/ClassesGrid.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { ClassItem } from "../types/Types";
 
 interface ClassListProps {
@@ -13,14 +8,24 @@ interface ClassListProps {
 
 const ClassesGrid: React.FC<ClassListProps> = ({ classList }) => {
   return (
-    <div className="flex gap-5">
+    <div className="flex w-full justify-between gap-5">
       {classList &&
         classList.map((classItem) => (
-          <Card key={classItem.id} className="w-80 shadow-lg">
-            <CardHeader>
-              <CardTitle>{classItem.className}</CardTitle>
+          <Card
+            key={classItem.id}
+            className="flex h-48 w-[350px] flex-col gap-2 shadow-lg transition-all duration-150 hover:scale-105 hover:shadow-xl"
+          >
+            <CardHeader className="h-fit rounded-t-lg bg-slate-500 px-0 pt-4">
+              <CardTitle className="flex flex-col items-center justify-center gap-2 text-slate-50">
+                <div className="w-5/6 overflow-x-clip text-ellipsis text-nowrap text-xl">
+                  {classItem.className}
+                </div>
+                <div className="text-xl font-normal">{classItem.classCode}</div>
+              </CardTitle>
             </CardHeader>
-            <CardDescription>{classItem.classCode}</CardDescription>
+            <div className="mb-4 mt-auto flex w-full items-center justify-center">
+              <button className="btn-sec btn-ghost w-40">Open</button>
+            </div>
           </Card>
         ))}
     </div>
