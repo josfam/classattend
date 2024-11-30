16b483fdca5c280ee5db1f186ee5d018eefc060d
josfam <jamukun@proton.me>
2024-11-30 23:21:52 +0300

    Make refetch times longer

--- frontend/src/utils/timings/timings.tsx
+++ frontend/src/utils/timings/timings.tsx
@@ -1,8 +1,8 @@
 // All times are in milliseconds
 
 const redirectDuration = 5000;
-const loginRefetchInterval = 100000;
+const loginRefetchInterval = 300000;
 const refetchInterval = 5000;
-const refetchStudentListInterval = 10000;
+const refetchStudentListInterval = 60000;
 
 export { redirectDuration, loginRefetchInterval, refetchInterval, refetchStudentListInterval };
