bb092ec2b3a771fe874952124af522f83d3e1fe9
josfam <jamukun@proton.me>
2024-11-30 15:49:55 +0300

    Add default empty value for pending student

--- backend/models/pending_student.py
+++ backend/models/pending_student.py
@@ -17,8 +17,8 @@ class PendingStudent(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    first_name = db.Column(db.String(120), nullable=False)
-    last_name = db.Column(db.String(120), nullable=False)
+    first_name = db.Column(db.String(120), nullable=False, default='')
+    last_name = db.Column(db.String(120), nullable=False, default='')
     email = db.Column(db.String(120), nullable=False)
     classroom_id = db.Column(
         db.Integer, db.ForeignKey('classrooms.id'), nullable=False
