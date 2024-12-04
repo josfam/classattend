a572be1ff2ca92a9b78472b23da3a512a2a44c7e
josfam <jamukun@proton.me>
2024-12-04 19:47:10 +0300

    Get attendance state of classroom from Zustand

--- frontend/src/roles/layouts/lecturer/components/TakeAttendanceBtn.tsx
+++ frontend/src/roles/layouts/lecturer/components/TakeAttendanceBtn.tsx
@@ -1,6 +1,7 @@
 import { ErrorToast, SuccessToast } from "@/components/Toasts";
 import { classroomApiPath } from "@/utils/urlPaths/apiPaths";
-import { useState } from "react";
+// import { useState } from "react";
+import useClassroomStore from "@/store/classroomStore";
 
 interface TakeAttendanceBtnProps {
   classHasStudents: undefined | boolean;
@@ -11,8 +12,16 @@ const TakeAttendanceBtn = ({
   classHasStudents,
   classId,
 }: TakeAttendanceBtnProps) => {
-  const [takingAttendanceNow, setTakingAttendanceNow] =
-    useState<boolean>(false);
+  // get attendance status from zustand
+  const attendanceStatus = useClassroomStore(
+    (currentState) => currentState.attendanceStatus,
+  );
+  const setAttendanceStatus = useClassroomStore(
+    (currentState) => currentState.setAttendanceOpen,
+  );
+
+  const takingAttendanceNow = attendanceStatus[classId];
+
   const handleAttendanceToggle = async () => {
     const response = await fetch(
       `${classroomApiPath}toggleAttendanceStatus/${classId}`,
@@ -26,7 +35,8 @@ const TakeAttendanceBtn = ({
     );
     const data = await response.json();
     if (response.ok) {
-      setTakingAttendanceNow(data.attendanceOpen);
+      // setTakingAttendanceNow(data.attendanceOpen);
+      setAttendanceStatus(classId, data.attendanceOpen);
       SuccessToast({ message: data.message });
     } else {
       ErrorToast({ message: data.message });
@@ -34,7 +44,7 @@ const TakeAttendanceBtn = ({
   };
   return (
     <button
-      className={`btn-pri w-52 text-nowrap ${!classHasStudents ? "border-0 bg-gray-400 hover:bg-gray-400" : ""} ${takingAttendanceNow}`}
+      className={`btn-pri w-52 text-nowrap ${!classHasStudents ? "border-0 bg-gray-400 hover:bg-gray-400" : ""} ${takingAttendanceNow ? "bg-emerald-500 text-emerald-900" : ""}`}
       disabled={!classHasStudents}
       onClick={handleAttendanceToggle}
     >
