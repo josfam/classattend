41d9f81cc03adfcac5d7e76fb5d9f90899389202
josfam <jamukun@proton.me>
2024-12-04 19:15:43 +0300

    Increase horizontal padding in layouts

--- frontend/src/roles/layouts/lecturer/LecturerLayout.tsx
+++ frontend/src/roles/layouts/lecturer/LecturerLayout.tsx
@@ -9,7 +9,7 @@ const LecturerLayout = () => {
       {/* Sidebar */}
       <LecturerSidebar />
       {/* Main content area */}
-      <main className="flex h-screen w-full flex-col items-center overflow-y-scroll bg-white px-8 pt-8">
+      <main className="flex h-screen w-full flex-col items-center overflow-y-scroll bg-white px-11 pt-8">
         <Outlet /> {/* Rendering nested routes */}
       </main>
     </div>
