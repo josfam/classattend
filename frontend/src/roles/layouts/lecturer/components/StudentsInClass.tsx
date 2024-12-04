2744969cc6ee47985c86515cf7ac155c1d3eecb2
josfam <jamukun@proton.me>
2024-12-04 20:23:43 +0300

    Fix contrast for pending students
    - Fix contrast between the background and foreground

--- frontend/src/roles/layouts/lecturer/components/StudentsInClass.tsx
+++ frontend/src/roles/layouts/lecturer/components/StudentsInClass.tsx
@@ -10,7 +10,7 @@ const StudentsInClass = ({ studentList }: studentListProps) => {
     <div className="mb-8 mt-8 flex h-fit w-full min-w-[350px] flex-col gap-3 rounded-lg border border-slate-300 bg-slate-50 px-8 py-8">
       {studentList.map((studentData) => (
         <div
-          className={`flex h-16 w-full items-center justify-start rounded-sm px-6 text-center font-medium transition-all duration-100 ${studentData.isPending ? "border border-gray-400 bg-gray-200 text-gray-500" : "border border-blue-400 bg-blue-200 text-sky-800 hover:font-semibold hover:shadow-sm hover:shadow-sky-300"} `}
+          className={`flex h-16 w-full items-center justify-start rounded-sm px-6 text-center font-medium transition-all duration-100 ${studentData.isPending ? "border border-gray-400 bg-gray-200 text-gray-600" : "border border-blue-400 bg-blue-200 text-sky-800 hover:font-semibold hover:shadow-sm hover:shadow-sky-300"} `}
           key={studentData.studentId}
         >
           <div className={`flex w-full justify-between text-lg`}>
@@ -19,7 +19,7 @@ const StudentsInClass = ({ studentList }: studentListProps) => {
                 <div>{studentData.firstName}</div>
                 <div>{studentData.lastName}</div>
               </div>
-              {studentData.isPending && <p>(No account)</p>}
+              {studentData.isPending && <p>No account</p>}
             </div>
             <div className="flex items-center text-xl">
               {!studentData.isPending && (
