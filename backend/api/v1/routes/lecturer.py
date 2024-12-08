0ae95dc05609edd6c98233ab5921c99200f488a4
josfam <jamukun@proton.me>
2024-12-08 10:30:05 +0300

    Remove debug prints from backend

--- backend/api/v1/routes/lecturer.py
+++ backend/api/v1/routes/lecturer.py
@@ -54,7 +54,6 @@ def add_classroom():
     '/getStudentList/<int:class_id>', methods=['GET'], strict_slashes=False
 )
 def get_student_list(class_id):
-    print('Class id', type(class_id), class_id)  # DEBUG
     student_classroom_data = (
         db.session.query(StudentClassroom).filter_by(class_id=class_id).first()
     )
