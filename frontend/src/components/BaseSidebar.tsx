ece844f80c996fa98a11ee3645be74cdba5a5eca
josfam <jamukun@proton.me>
2024-12-10 18:16:35 +0300

    Move components to outer most `components` dir

--- frontend/src/components/BaseSidebar.tsx
+++ frontend/src/components/BaseSidebar.tsx
@@ -7,7 +7,7 @@ interface BaseSidebarProps {
 
 const BaseSidebar = ({ children }: BaseSidebarProps) => {
   return (
-    <nav className="flex h-screen min-w-48 flex-col overflow-hidden bg-sky-700">
+    <nav className="hidden h-screen min-w-48 flex-col overflow-hidden bg-sky-700 lg:flex">
       {children} {/* Render child components */}
       <div className="mt-auto">
         <LogoutBtn />
