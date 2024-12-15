2cf3fb27a987833ecabed824377cf44c1dd85667
josfam <jamukun@proton.me>
2024-12-15 18:39:45 +0300

    Fix: Store hashed password as bytes for postgresql

--- backend/models/user.py
+++ backend/models/user.py
@@ -25,7 +25,7 @@ class User(db.Model):
     last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     role = db.Column(db.Enum(UserRole), nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    password_hash = db.Column(db.LargeBinary, nullable=False)
 
     # relationship to a student
     student = db.relationship('Student', back_populates='user', uselist=False)
